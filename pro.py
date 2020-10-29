cc24601a39cbeb7838b56530d97363e49a194b1d
Kim <fkqoseka@gmail.com>
2020-10-29 10:29:05 +0900

    morning

--- pro.py
+++ pro.py
@@ -1,41 +1,44 @@
 from konlpy.tag import Okt
 import json
 import os
-from pprint import pprint
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
 
-def read_data(filename):
-    with open(filename, 'r') as f:
-        data = [line.split('\t') for line in f.read().splitlines()]
-        data = data[1:]
-    return data
+train_data = pd.read_table('ratings_train.txt') # 데이터 읽어오기
+test_data = pd.read_table('ratings_test.txt')
 
-train_data = read_data('ratings_train.txt')
-test_data = read_data('ratings_test.txt')
+# print(type(train_data)) # data들의 type은 dataframe이다.
+print('훈련용 리뷰 개수 :',len(train_data))
+print('훈련용 리뷰 개수 :',len(test_data))
 
-# print(len(train_data))
-# print(len(test_data))
-# print(len(train_data[0]))
-# print(len(test_data[0]))
+print()
 
-okt = Okt()
-# print(okt.pos(u'우린 서로 너무도 다른 세상을 살아왔죠 한 번 스쳐 지났을 뿐'))
+# print(train_data[:5]) # 상위 5개 샘플 출력
 
-def tokenize(doc):
-    # norm은 정규화, stem은 근어로 표시하기를 나타냄
-    return ['/'.join(t) for t in okt.pos(doc, norm=True, stem=True)]
+# ID는 분석에 영향을 주지 않으므로 앞으로 무시함
+dup_doc = len(train_data['document']) - train_data['document'].nunique()
+dup_label = train_data['label'].nunique()
+# print(dup_doc) 
+# print(dup_label)
+print()
+print(f"중복 Document : {dup_doc}개, 중복 Label {dup_label}개") # 중복 여부 확인
 
-if os.path.isfile('train_docs.json'):
-    with open('train_docs.json') as f:
-        train_docs = json.load(f)
-    with open('test_docs.json') as f:
-        test_docs = json.load(f)
-        
-else:
-    train_docs = [(tokenize(row[1]), row[2]) for row in train_data]
-    test_docs = [(tokenize(row[1]), row[2]) for row in test_docs ]
-    # JSON 파일로 저장
-    with open('train_docs.json', 'w', encoding='utf-8') as make_file:
-        json.dump(train_docs, make_file, ensure_ascii=False, indent='\t')
-        
-    with open('test_docs.json', 'w', encoding='utf-8') as make_file:
-        json.dump(test_docs, make_file, ensure_ascii=False, indent = '\t')
+train_data.drop_duplicates(subset=['document'], inplace=True) # 중복 샘플 제거
+print()
+print('총 샘플의 수 :',len(train_data))
+
+train_data['label'].value_counts().plot(kind = 'bar') # label 값의 분포
+# plt.show()
+print()
+print(train_data.groupby('label').size().reset_index(name = 'count')) # 정확한 갯수 확인
+print()
+print(f'Null in value : {train_data.isnull().values.any()}') # 데이터에 Null 값이 있는지 확인
+print(train_data.isnull().sum())
+print()
+print(f'Find where the null is: \n{train_data.loc[train_data.document.isnull()]}')
+
+train_data = train_data.dropna(how = 'any') # Null 값이 존재하는 행 제거
+print(f'Null in value : {train_data.isnull().values.any()}') # Null 이존재하는지 재 확인
+print('총 샘플의 수 :',len(train_data))
