b7232277384fe57bce7a4b33bc00cd897a0cc4bc
Kim <fkqoseka@gmail.com>
2020-11-03 01:46:53 +0900

    so far not bad

--- pro.py
+++ pro.py
@@ -78,8 +78,8 @@ print('전처리 후 테스트용 샘플의 개수 :',len(test_data))
 stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
 
 # 데이터 양이 너무 많아서 우선 300개정도 크기 조정
-train_data = train_data[:100]
-test_data = test_data[:100]
+train_data = train_data[:1000]
+test_data = test_data[:1000]
 
 okt = Okt()
 X_train = []
@@ -121,11 +121,57 @@ for key, value in tokenizer.word_counts.items():
     if (value < threshold) : 
         rare_cnt = rare_cnt + 1
         rare_freq = rare_freq + value
-        rare_freq = rare_freq + value
         
 print('단어 집합(Vocabulary)의 크기 :', total_cnt)
 print('등장 빈도가 %s 번 이하인 희귀 단어의 수: %s' %(threshold - 1, rare_cnt))
 print('단어 집합에서 희귀 단어의 비율:', (rare_cnt / total_cnt) * 100)
 print('전체 등장 빈도에서 희귀 단어 등장 빈도 비율', (rare_freq / total_freq) * 100)
 
+# 전체 단어 개수 중 빈도수 1(원래는 2, 지금은 데이터 양을 줄여서 1) 이하인 단어 개수는 제거.
+# 0번 패딩 토큰과 1번 00V 토큰을 고려하여 + 2
+vocab_size = total_cnt - rare_cnt + 2
+print('단어 집합의 크기:', vocab_size)
 
+# 케라스 토크나이저의 인자로 넘겨줌 -> 텍스트 시퀀스를 숫자 시퀀스로 변환
+tokenizer = Tokenizer(vocab_size, oov_token = '00V')
+tokenizer.fit_on_texts(X_train)
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+
+print(f'After Tokenizing: {X_train[:3]}')
+
+# train_data에서 y_train과 y_test를 별도로 저장해줌.
+y_train = np.array(train_data['label'])
+y_test = np.array(test_data['label'])
+
+# 빈 샘플들의 인덱스 저장
+drop_train = [index for index, sentence in enumerate(X_train) if len(sentence) < 1]
+
+# 빈 샘플 제거
+X_train = np.delete(X_train, drop_train, axis = 0)
+y_train = np.delete(y_train, drop_train, axis = 0)
+print()
+print('빈 샘플 제거 이후 샘플 수 :')
+print(len(X_train))
+print(len(y_train))
+
+# 패딩 (서로 다른 길이의 샘플들의 길이를 동일하게 맞춰주는 작업)
+print('리뷰의 최대 길이 :',max(len(l) for l in X_train))
+print('리뷰의 평균 길이 :',sum(map(len, X_train))/len(X_train))
+plt.hist([len(s) for s in X_train], bins=50)
+plt.xlabel('length of samples')
+plt.ylabel('number of samples')
+plt.show()
+
+def below_threshold_len(max_len, nested_list):
+    cnt = 0
+    for s in nested_list:
+        if(len(s) <= max_len):
+            cnt = cnt + 1
+    print('전체 샘플 중 길이가 %s 이하인 샘플의 비율: %s'%(max_len, (cnt / len(nested_list))*100))
+            
+max_len = 30
+below_threshold_len(max_len, X_train)
+
+X_train = pad_sequences(X_train, maxlen = max_len)
+X_test = pad_sequences(X_test, maxlen = max_len)
